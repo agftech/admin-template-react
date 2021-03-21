4bf8dfcf941ae6e94e6b50b341cc37a76f171d88
agftech <agiraof@gmail.com>
2021-03-21 14:14:23 -0700

    :package: add form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,54 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
+import {
+  Button,
+  Card,
+  CardBody,
+  Col,
+  Container,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Row,
+} from "reactstrap";
+
 export default class Login extends Component {
   render() {
     return (
-      <div>
-        <h1>Login</h1>
-        <Link to="">Home</Link>
+      <div className="login mt-5 flex-row align-items-center">
+        <Container>
+          <Row className="justify-content-center">
+            <Col md="6">
+              <Card>
+                <CardBody>
+                  <Form>
+                    <h1>Login</h1>
+                    <FormGroup className="mb-3">
+                      <Label for="email">Email</Label>
+                      <Input type="email" id="email" placeholder="E-mail" />
+                    </FormGroup>
+                    <FormGroup className="mb-3">
+                      <Label for="password">Password</Label>
+                      <Input
+                        type="password"
+                        id="password"
+                        placeholder="Password"
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Button outline color="primary">
+                        Go!
+                      </Button>
+                    </FormGroup>
+                  </Form>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+          <Link to="">Back Home!</Link>
+        </Container>
       </div>
     );
   }
