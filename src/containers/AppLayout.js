09ee9376f2540901eae7a6275106979dc2606558
agftech <agiraof@gmail.com>
2021-03-22 07:23:46 -0700

    :package:add header component

--- src/containers/AppLayout.js
+++ src/containers/AppLayout.js
@@ -1,20 +1,24 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import Templates from "./routes";
+import Header from './../components/Header';
 
 export default class AppLayout extends Component {
   render() {
     return (
-      <div>
-        <Switch>
-          {Templates.map((route) => (
-            <Route
-              path={route.path}
-              exact
-              render={(props) => <route.template {...props} />}
-            />
-          ))}
-        </Switch>
+      <div id="main-wrapper">
+        <div id="content-wrapper">
+          <Header />
+          <Switch>
+            {Templates.map((route) => (
+              <Route
+                path={route.path}
+                exact
+                render={(props) => <route.template {...props} />}
+              />
+            ))}
+          </Switch>
+        </div>
       </div>
     );
   }
