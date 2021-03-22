f3e5810a3bf098cad7cee045e00d220079ee5f2c
agftech <agiraof@gmail.com>
2021-03-22 08:48:28 -0700

    :package: make sidebar dynamic

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Templates from './../containers/routes';
 
 export default function Sidebar(props) {
   return (
@@ -7,7 +8,10 @@ export default function Sidebar(props) {
         <h2>School of net</h2>
       </div>
       <div className="list-group">
-        <a href="" className="list-group-item"></a>
+          <a href="/" className="list-group-item">Home</a>
+          { Templates.map((template, index) => (                    
+              <a href={template.path} className="list-group-item">{template.name}</a>
+          ))}
       </div>
     </aside>
   );
