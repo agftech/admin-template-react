1bfe7961375d7a460fef5a982021f8e7a2fc02d2
agftech <agiraof@gmail.com>
2021-03-21 17:41:10 -0700

    :package: add context to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Home from "./components/Home";
+import AppLayout from "./containers/AppLayout";
 import Login from "./components/Login";
 import Forgot from "./components/Forgot";
 import Register from "./components/Register";
@@ -11,7 +11,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" name="Home" render={props => <Home {...props} />} />
+        <Route exact path="/" name="AppLayout" render={props => <AppLayout {...props} />} />
         <Route exact path="/login" name="Login" render={props => <Login {...props} />} />
         <Route exact path="/forgot" name="Forget" render={props => <Forgot {...props} />} />          
         <Route exact path="/register" name="Register" render={props => <Register {...props} />} />
--- src/containers/AppLayout.js
+++ src/containers/AppLayout.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-export default class Home extends Component {
+export default class AppLayout extends Component {
   render(props) {
     return (
       <div>
-        <h1>Home</h1>
+        <h1>AppLayout</h1>
         <Link to="/login">Login</Link>
       </div>
     );
