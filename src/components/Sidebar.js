a227d571cee083ee5af3c7ba163e8bb1e0b8177c
agftech <agiraof@gmail.com>
2021-03-22 08:35:27 -0700

    :package:add sidebar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Sidebar(props) {
+  return (
+    <aside id="sidebar">
+      <div className="sidebar-title">
+        <h2>School of net</h2>
+      </div>
+      <div className="list-group">
+        <a href="" className="list-group-item"></a>
+      </div>
+    </aside>
+  );
+}
--- src/containers/AppLayout.js
+++ src/containers/AppLayout.js
@@ -1,17 +1,20 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import Templates from "./routes";
-import Header from './../components/Header';
+import Header from "./../components/Header";
+import Sidebar from "./../components/Sidebar";
 
 export default class AppLayout extends Component {
   render() {
     return (
-      <div id="main-wrapper">
+      <div className="d-flex" id="main-wrapper">
+        <Sidebar />
         <div id="content-wrapper">
           <Header />
           <Switch>
-            {Templates.map((route) => (
+            {Templates.map((route, index) => (
               <Route
+                key={index}
                 path={route.path}
                 exact
                 render={(props) => <route.template {...props} />}
