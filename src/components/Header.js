df4f663e3a9ec1a705747dadd306e6d8e23a663c
agftech <agiraof@gmail.com>
2021-03-22 10:44:47 -0700

    :package: feat: add handleClickToSidebar function

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,19 @@ import {
 } from 'reactstrap';
 
 export default function Header(props) {
+
+    function handleClickToSidebar(e) {
+        e.preventDefault();
+        document.getElementById('main-wrapper').classList.toggle('toggled');
+    }
+
     return(
         <header>
             <Navbar color="light" light>
                 <NavbarBrand href="/">
                     <img src="https://via.placeholder.com/150x50" alt=""/>
                 </NavbarBrand>
-                <NavbarToggler />
+                <NavbarToggler onClick={handleClickToSidebar} />
             </Navbar>
         </header>
     );
